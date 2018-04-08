814793fbbc67e9450a273c72b31193039766904f
RAHUL THAKUR <iamrahulthakur.1992@gmail.com>
2018-04-08 17:18:21 +0530

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,6 @@
 var socket = io();
 socket.on('connect',function(){
     console.log('Connected to server');
-    socket.emit('createMessage',{
-        from:'iamrahulthakur',
-        text:'Hey. That work\s for me.'
-    });
 });
 socket.on('disconnect',function(){
     console.log('Disconnected from server');
